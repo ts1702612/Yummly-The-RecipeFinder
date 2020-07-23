94ddda0464620cbd4b146e7e857591a877059b0a
taruna <tarunaagrawal2017@gmail.com>
2020-07-23 19:44:28 +0530

    Updates

---
+++ precache-manifest.09ac5ba464eae73ed763fc4e3e48aba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd0802ed78f7e527a9c133de47b4828",
+    "url": "/Yummly-The-RecipeFinder/index.html"
+  },
+  {
+    "revision": "520f2a0829c7ea4bbe34",
+    "url": "/Yummly-The-RecipeFinder/static/css/main.ff83362f.chunk.css"
+  },
+  {
+    "revision": "7753739649a2938da221",
+    "url": "/Yummly-The-RecipeFinder/static/js/2.3c795e18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Yummly-The-RecipeFinder/static/js/2.3c795e18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520f2a0829c7ea4bbe34",
+    "url": "/Yummly-The-RecipeFinder/static/js/main.96dd41a1.chunk.js"
+  },
+  {
+    "revision": "fc64e0e2c31f6fdc8d72",
+    "url": "/Yummly-The-RecipeFinder/static/js/runtime-main.53d0f75a.js"
+  }
+]);
